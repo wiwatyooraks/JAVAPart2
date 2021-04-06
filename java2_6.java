bb0288a597aefd189e305fc035f1d04437fbdad4
wiwatyooraks <78940910+wiwatyooraks@users.noreply.github.com>
2021-04-06 18:10:27 +0700

    JAVA2_6

---
+++ java2_6.java
@@ -0,0 +1,9 @@
+package com.wiwatyooraks.java2;
+
+public class java2_6 {
+	public static void main(String[] args) {
+		char[] myChar = { 'C', 'h', 'a', 'r', 'z', ' ', '3', '6', '5' };
+		String myWord = new String(myChar);
+		System.out.println(myWord);
+	}
+}
